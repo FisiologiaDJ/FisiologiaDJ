776266a868e98f9c8f22f13649a230d56769d727
FisiologiaDJ <fdjcontacto@gmail.com>
2026-05-02 17:14:02 -0600

    Update potenequi_app.py

--- potenequi_app.py
+++ potenequi_app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Potencial de equilibrio iónico", page_icon="⚡"
 st.markdown("<h3 style='text-align: center;'>⚡Calculadora del potencial de equilibrio iónico⚡      (Ecuación de Nernst)</h3>", unsafe_allow_html=True)
 
 st.info("💡¿Sabias que la ecuación de Nernst es de gran utilidad en fisiología para calcular el potencial de equilibrio iónico? Este valor representa la energía eléctrica que compensa a la energía química, esto nos ayuda a entender mejor el potencial de membrana en reposo y el potencial de acción. ⚡Cuando se alcanza el voltaje del potencial de equilibrio iónico el flujo neto del ion es cero.")
-st.info("📚Intrucciones: Elige el ion de tu interés, tienes para elegir entre sodio, potasio, cloro y calcio. Después elige la concentración extra e intracelular, así como la temperatura de tu preferencia desplazando las barras hasta obtener el valor deseado. 🤩Observa cómo se actualiza el potencial de equilibrio iónico resultante en la gráfica y en valor numérico en milivolts (mV) en la parte inferior. Dejamos para ti la ecuación de Nernst al final.")
+st.info("📚Instrucciones: Elige el ion de tu interés, puedes elegir entre los iones sodio, potasio, cloro y calcio. Después elige la concentración extra e intracelular, así como la temperatura de tu preferencia desplazando las barras hasta obtener el valor deseado. 🤩Observa cómo se actualiza el potencial de equilibrio iónico resultante en la gráfica y en valor numérico en milivolts (mV). Dejamos para ti la ecuación de Nernst al final.")
 
 ion = st.selectbox("Ion", ["Na⁺", "K⁺", "Cl⁻", "Ca²⁺"])
 
